916c2a46f494378851ff3907552f0024904b02b0
Ilan Schnell <ilanschnell@gmail.com>
2013-03-24 02:48:08 -0500

    update Development Status

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     url = "https://github.com/ilanschnell/pycosat",
     license = "MIT",
     classifiers = [
-        "Development Status :: 1 - Planning",
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: C",
