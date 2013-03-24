3dd64d6f8b7fbd1ef20fa52f3030a9713e80212a
Ilan Schnell <ilanschnell@gmail.com>
2013-03-24 17:09:23 -0500

    add call to missing descructor picosat_reset()

--- pycosat.c
+++ pycosat.c
@@ -67,7 +67,7 @@ static PyObject* solve(PyObject* self, PyObject* args)
 {
     PicoSAT *picosat;
     PyObject *clauses;          /* list of clauses */
-    PyObject *list;             /* return value if satisfiable */
+    PyObject *result;           /* return value */
     Py_ssize_t max_idx, i;
     int res, val, vars, verbose = 0;
 
@@ -89,29 +89,32 @@ static PyObject* solve(PyObject* self, PyObject* args)
     switch (res) {
     case PICOSAT_SATISFIABLE:
         max_idx = picosat_variables(picosat);
-        list = PyList_New(max_idx);
-        if (list == NULL)
+        result = PyList_New(max_idx);
+        if (result == NULL)
             return NULL;
         for (i = 1; i <= max_idx; i++) {
             val = picosat_deref(picosat, i);
             assert(val == -1 || val == 1);
-            if (PyList_SetItem(list, i - 1,
+            if (PyList_SetItem(result, i - 1,
                                PyBool_FromLong(val < 0 ? 0 : 1)) < 0)
                 return NULL;
         }
-        return list;
-
+	break;
     case PICOSAT_UNSATISFIABLE:
-        Py_RETURN_FALSE;
-
+        Py_INCREF(Py_False);
+        result = Py_False;
+	break;
     case PICOSAT_UNKNOWN:
-        Py_RETURN_NONE;
-
+        Py_INCREF(Py_None);
+        result = Py_None;
+	break;
     default:
         PyErr_Format(PyExc_SystemError,
                      "unknown picosat return value: %d", res);
-        return NULL;
     }
+
+    picosat_reset(picosat);
+    return result;
 }
 
 
