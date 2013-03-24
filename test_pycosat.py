182fa87ebb013c71744566bc0fe693ccef8df251
Ilan Schnell <ilanschnell@gmail.com>
2013-03-24 02:57:52 -0500

    add unit test suite

--- test_pycosat.py
+++ test_pycosat.py
@@ -1,18 +1,36 @@
+import unittest
+
 import pycosat
 
-print pycosat.solve(
-    5, [
-    [1, -5, 4],
-    [-1, 5, 3, 4],
-    [-3, -4],
-    ],
-#    True
-)
 
-print pycosat.solve(
-    2, [
-    [-1],
-    [1],
-    ],
-#    True
-)
+tests = []
+
+class TestSolver(unittest.TestCase):
+
+    def test_sat_1(self):
+        res = pycosat.solve(5, [[1, -5, 4],
+                                [-1, 5, 3, 4],
+                                [-3, -4]])
+        self.assertEqual(res, [True, False, False, False, True])
+
+    def test_unsat_2(self):
+        res = pycosat.solve(2, [[-1],
+                                [1]])
+        self.assertEqual(res, False)
+
+tests.append(TestSolver)
+
+# ------------------------------------------------------------------------
+
+def run(verbosity=1, repeat=1):
+    suite = unittest.TestSuite()
+    for cls in tests:
+        for _ in range(repeat):
+            suite.addTest(unittest.makeSuite(cls))
+
+    runner = unittest.TextTestRunner(verbosity=verbosity)
+    return runner.run(suite)
+
+
+if __name__ == '__main__':
+    run()
