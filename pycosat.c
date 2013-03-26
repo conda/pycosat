bb541175bb826f1130d2c35a27f08210c4ccf0d6
Travis E. Oliphant <teoliphant@gmail.com>
2013-03-26 00:19:31 -0500

    Add the ability to find all the solutions.

--- pycosat.c
+++ pycosat.c
@@ -38,6 +38,23 @@ static void py_free(void *mmgr, void *ptr, size_t bytes) {
 }
 #endif
 
+static void
+add_solution(PicoSAT * picosat, PyObject* new, char *mem, int max_idx)
+{
+    int i, var;
+
+    for (i = 1; i <= max_idx; i++)
+        mem[i] = (picosat_deref (picosat, i) > 0) ? 1 : -1;
+
+    for (i = 1; i <= max_idx; i++) {
+        var = (mem[i] < 0) ? i : -i;
+        picosat_add(picosat, var);
+        PyList_SET_ITEM(new, (Py_ssize_t) (i-1),
+                        PyInt_FromLong((long) var));
+    }
+    picosat_add (picosat, 0);
+
+}
 
 static int add_clause(PicoSAT *picosat, PyObject *clause)
 {
@@ -86,14 +103,16 @@ static int add_clauses(PicoSAT *picosat, PyObject *clauses)
     return 0;
 }
 
-static PyObject* solve(PyObject* self, PyObject* args)
+static PyObject* solve_all(PyObject *self, PyObject *args)
 {
     PicoSAT *picosat;
     PyObject *clauses;          /* list of clauses */
-    PyObject *result = NULL;    /* return value */
-    int res, val, max_idx, i, vars, verbose = 0;
+    PyObject *result = NULL;    /* all solutions */
+    PyObject *new = NULL;       /* one of the solutions */
+    int res, max_idx, vars, sols=0, verbose = 0;
+    char *mem=NULL;
 
-    if (!PyArg_ParseTuple(args, "iO|i:verbose", &vars, &clauses, &verbose))
+    if (!PyArg_ParseTuple(args, "iO|i:solve", &vars, &clauses, &verbose))
         return NULL;
 
 #ifdef WITH_PYMEM
@@ -104,8 +123,88 @@ static PyObject* solve(PyObject* self, PyObject* args)
     picosat_set_verbosity(picosat, verbose);
 
     picosat_adjust(picosat, vars);
-    if (add_clauses(picosat, clauses) < 0)
+    if (add_clauses(picosat, clauses) < 0) {
+        picosat_reset(picosat);
         return NULL;
+    }
+
+    if (verbose >= 2)
+        picosat_print(picosat, stdout);
+
+    result = PyList_New(0);  /* If this fails something is seriously wrong */
+
+ NEXT_SOLUTION:
+
+    Py_BEGIN_ALLOW_THREADS  /* release GIL */
+    res = picosat_sat(picosat, -1);
+    Py_END_ALLOW_THREADS
+
+    if (res == PICOSAT_SATISFIABLE) {
+        sols++;
+        if (verbose >= 2) {
+            fprintf(stdout, "%d solutions so far\n", sols);
+        }
+        max_idx = picosat_variables (picosat);
+        if (!mem) { /* Temporary storage */
+            mem = calloc(max_idx+1, 1);
+        }
+        new = PyList_New(max_idx);
+        if ((new == NULL) || (PyList_Append(result, new) < 0)) {
+            Py_XDECREF(new);
+            Py_DECREF(result);
+            result = NULL;
+            goto final;
+        }
+        /* Move solution to the list and to constraints 
+           so that next solution will be generated */
+        add_solution(picosat, new, mem, max_idx);
+        Py_DECREF(new);
+        goto NEXT_SOLUTION;
+    }
+    else if (res == PICOSAT_UNSATISFIABLE) {
+        if (verbose >= 2) {
+            fprintf(stdout, "%d total solutions\n", sols);
+        }
+    }
+    else if (res == PICOSAT_UNKNOWN) {
+        new = PyString_FromFormat("limit reached after %d solutions", sols);
+        PyList_Append(result, new);
+        Py_XDECREF(new);
+    }
+    else {
+        new = PyString_FromFormat("unknown picosat return value %d after %d solutions\n", res, sols);
+        PyList_Append(result, new);
+        Py_XDECREF(new);
+    }
+
+ final:
+    if (!mem) free(mem);
+    picosat_reset(picosat);
+    return result;
+}
+
+static PyObject* solve(PyObject* self, PyObject* args)
+{
+    PicoSAT *picosat;
+    PyObject *clauses;          /* list of clauses */
+    PyObject *result = NULL;    /* return value */
+    int res, val, max_idx, i, vars, verbose = 0;
+
+    if (!PyArg_ParseTuple(args, "iO|i:solve", &vars, &clauses, &verbose))
+        return NULL;
+
+#ifdef WITH_PYMEM
+    picosat = picosat_minit(NULL, py_malloc, py_realloc, py_free);
+#else
+    picosat = picosat_init();
+#endif
+    picosat_set_verbosity(picosat, verbose);
+
+    picosat_adjust(picosat, vars);
+    if (add_clauses(picosat, clauses) < 0) {
+        picosat_reset(picosat);
+        return NULL;
+    }
 
     if (verbose >= 2)
         picosat_print(picosat, stdout);
@@ -118,14 +217,19 @@ static PyObject* solve(PyObject* self, PyObject* args)
     case PICOSAT_SATISFIABLE:
         max_idx = picosat_variables(picosat);
         result = PyList_New((Py_ssize_t) max_idx);
-        if (result == NULL)
+        if (result == NULL) {
+            picosat_reset(picosat);
             return NULL;
+        }
         for (i = 1; i <= max_idx; i++) {
             val = picosat_deref(picosat, i);
             assert(val == -1 || val == 1);
             if (PyList_SetItem(result, (Py_ssize_t) i - 1,
-                               PyInt_FromLong((long) val * i)) < 0)
+                               PyInt_FromLong((long) val * i)) < 0) {
+                Py_DECREF(result);
+                picosat_reset(picosat);
                 return NULL;
+            }
         }
         break;
 
@@ -150,6 +254,7 @@ static PyObject* solve(PyObject* self, PyObject* args)
 /* declaration of methods supported by this module */
 static PyMethodDef module_functions[] = {
     {"solve", (PyCFunction) solve, METH_VARARGS},
+    {"solveall", (PyCFunction) solve_all, METH_VARARGS},
     {NULL,    NULL}  /* sentinel */
 };
 
