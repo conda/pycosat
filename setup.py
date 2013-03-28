95d079ef315a40480f569c7b7d780f4f06c2db44
Ilan Schnell <ilanschnell@gmail.com>
2013-03-28 02:12:32 -0500

    update development status

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     url = "https://github.com/ContinuumIO/pycosat",
     license = "MIT",
     classifiers = [
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: C",
