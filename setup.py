9c20f09fc5b19158f553beece2747288160b86ab
Ilan Schnell <ilanschnell@gmail.com>
2014-06-10 00:00:15 -0500

    Python 3.4 is also supported now

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Utilities",
     ],
     ext_modules = [Extension(**ext_kwds)],
