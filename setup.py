fe1bbdd6a25ee3bd7800b7341d67e1bfcebb17d5
Ilan Schnell <ilanschnell@gmail.com>
2016-01-03 15:18:57 +0100

    add official Python 3.5 support

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Utilities",
     ],
     ext_modules = [Extension(**ext_kwds)],
