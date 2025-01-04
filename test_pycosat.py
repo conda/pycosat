2216f742457dcf312b79d8f32b868e4a435931dc
Bradley Dice <bdice@bradleydice.com>
2025-01-04 12:35:24 -0600

    Fix tests on Python 3.13

--- test_pycosat.py
+++ test_pycosat.py
@@ -258,7 +258,7 @@ def run(verbosity=1, repeat=1):
     suite = unittest.TestSuite()
     for cls in tests:
         for _ in range(repeat):
-            suite.addTest(unittest.makeSuite(cls))
+            suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(cls))
 
     runner = unittest.TextTestRunner(verbosity=verbosity)
     return runner.run(suite)
